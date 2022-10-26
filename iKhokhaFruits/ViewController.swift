91bce4164746773af4b524c6e316ed40ddbba348
wykee2 <git.postagic@gmail.com>
2022-10-26 20:27:42 +0300

    Initial Commit

---
+++ iKhokhaFruits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKhokhaFruits
+//
+//  Created by Wykee Njenga on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
