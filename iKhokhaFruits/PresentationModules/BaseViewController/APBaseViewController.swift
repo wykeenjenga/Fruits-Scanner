36a194ee96f1c66cc09dc59645ce994aff61c422
wykee2 <wycliffnjenga19@gmail.com>
2022-10-30 12:44:43 +0300

    Created App BaseViewController
    Base controller will help us to implement our HUD loading dialog among others

---
+++ iKhokhaFruits/PresentationModules/BaseViewController/APBaseViewController.swift
@@ -0,0 +1,12 @@
+//
+//  APBaseViewController.swift
+//  iKhokhaFruits
+//
+//  Created by Wykee Njenga on 10/30/22.
+//  Copyright © 2022 iKhokha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class BaseViewController: UIViewController, HUDLoaderProtocol, StoryboardInstantiable { }
