7081339015b0ad54c275bea8467c7dd974b5d692
wykee2 <wycliffnjenga19@gmail.com>
2022-10-30 14:47:33 +0300

    Created hardwall view controller
    Hard wall includes , sign up / in storyboard

---
+++ iKhokhaFruits/PresentationModules/HardWallModule/ViewController/APHardWallViewController.swift
@@ -0,0 +1,30 @@
+//
+//  APHardWallViewController.swift
+//  iKhokhaFruits
+//
+//  Created by Wykee Njenga on 10/30/22.
+//  Copyright © 2022 iKhokha. All rights reserved.
+//
+
+import UIKit
+
+class APHardWallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
